05b39f5565fc332439c9e1d4856461fb4f581b2b
zdrouse <zdrouse@gmail.com>
2020-02-01 22:53:54 -0500

    updates

--- file_scanner.py
+++ file_scanner.py
@@ -1,4 +1,4 @@
-import os, io, argparse, re, fnmatch
+import os, io, argparse, re, fnmatch, hashlib, sys
 
 # script path
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -11,11 +11,25 @@ def match_rex(file, rex_items):
         if match:
             return True
 
+def calculate_hash(file):
+    try:
+        with open(file, "rb") as f:
+            f_bytes = f.read()
+            f_hash = hashlib.sha256(f_bytes).hexdigest()
+            return f_hash
+    except Exception as e:
+        print(f"Could not read file for hash: {e}... Exiting.")
+        sys.exit(1)
+
 def find_files(dir_path, rex_items):
+    print(rex_items)
     for dir_path, sub_dir_list, file_list in os.walk(dir_path):
         for file in file_list:
             file_name = os.path.abspath(file)
-            print(file_name)
+            #print(file_name)
+            if match_rex(file_name, rex_items):
+                file_hash = calculate_hash(file_name)
+                print(f"{file_name} - {file_hash}")
 
 def main():
     # arg parser
@@ -25,9 +39,14 @@ def main():
     parser.add_argument('-i', '--src', help='Source folder to scan', required=True, type=str)
     parser.add_argument('-f', '--patfile', default='string', help='Patterns file')
     parser.add_argument('-o', '--dest', help='Output folder', required=True, type=str)
+    # parse the arguments
     args = parser.parse_args()
-    print(args.src)
-    #find_files(dir_path, rex_items)
+    # check if there are any regexps
+    if len(args.regexps) == 0:
+        print("You must specify patterns in command line or as a file with -f option.")
+        sys.exit(1)
+
+    find_files(args.src, args.regexps)
 
 
 if __name__ == "__main__":
